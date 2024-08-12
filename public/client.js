f31eb94545bbbce23f700fae0ca8c2c5a74fe268
agr555 <agr555g@gmail.com>
2024-08-12 12:05:24 +0200

    pressure

---
+++ public/client.js
@@ -0,0 +1,69 @@
+
+console.log('Start')
+let loadButton = document.getElementById('load')
+
+loadButton.onclick = async () => {
+
+    async function success(position) {
+        const lat = position.coords.latitude;//53.13; //
+        const lon = position.coords.longitude;//4.11;//
+        const lang = 'en'
+        // const lat = 50.45; //
+        // const lon = 1.614852;//
+        let result = await fetch(`/api/weather1?lat=${lat}&lon=${lon}&lang=${lang}&units=metric`);  
+        let resJson = await result.json();
+        // console.log(resJson);
+
+        let name = document.getElementById('name');
+        name.innerText = resJson.city;
+
+        let temp = document.getElementById('temp');
+        temp.innerText = resJson.temp;
+
+        let max_temp = document.getElementById('max_temp'); 
+        max_temp.innerText = resJson.temp_max;
+
+        let min_temp = document.getElementById('min_temp'); 
+        min_temp.innerText = resJson.temp_min;
+
+        let pressure = document.getElementById('pressure'); 
+        pressure.innerText = resJson.pressure;
+
+        let humidity = document.getElementById('humidity'); 
+        humidity.innerText = resJson.humidity;
+
+        let icon = document.getElementById('icon');       
+        icon.src =  `https://openweathermap.org/img/wn/${resJson.icon}@2x.png`;
+
+        let weatherdesc = document.getElementById('weatherdesc');
+        weatherdesc.innerText = resJson.weatherdesc;
+
+        let winddeg = document.getElementById('winddeg');        
+        winddeg.innerText = resJson.winddeg;
+
+        let windspeed = document.getElementById('windspeed');
+        windspeed.innerText = resJson.windspeed;
+
+        let rain = document.getElementById('rain');  
+        rain.innerText = resJson.rain?resJson.rain:'';
+
+        let snow = document.getElementById('snow');  
+        snow.innerText = resJson.rain?resJson.snow:'';
+
+        status.textContent = "";
+    }
+
+    function error() {
+        status.textContent = "Unable to retrieve your location";
+    }
+
+    if (!navigator.geolocation) {
+        status.textContent = "Geolocation is not supported by your browser";
+    } else {
+        status.textContent = "Locating…";
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+}
+
+
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express')
+
+const mongoose = require('mongoose');
+require('dotenv').config()
+// console.log(process.env.MONGODB_URI)
+
+// mongoose.connect('mongodb://127.0.0.1:27017/weatherdb');//create db or/and connect
+mongoose.connect(process.env.MONGODB_URI);//create db or/and connect
+const Weather1 = mongoose.model('Weather1',
+  {
+    city: String, // create table and/or open
+    temperature: String,
+    date: String,
+    rain: String,
+    snow: String,
+    pressure: String
+  });
+
+const Okanswer = mongoose.model('Okanswer',
+  {
+    name: String, // create table and/or open
+    phone: String,
+    choice: String,
+  });
+
+const app = express()
+const port = 5000
+
+app.use('/', express.static('public'))
+
+http://localhost:5000/data?lang=ru&temperature=C&lon=55&lat=44&units=metric
+app.get('/api/weather1', async (req, res) => {
+  let lat = req.query.lat
+  let lon = req.query.lon
+  let lang = req.query.lang
+  let units = req.query.units
+  let date = new Date()
+  if (lat && lon && lang && units) {
+    let resWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${lang}&units=${units}&appid=7c871f23791e7e4646bb4bf648aca357`);
+    // console.log(resWeather )
+    let resWeatherJson = await resWeather.json()
+    // console.log(resWeatherJson)
+    res.json({  //после запроса ответ на фронтентенд
+      'city': resWeatherJson.name,
+      'temp': (resWeatherJson.main.temp).toFixed(2) + '°',
+      'temp_max': (resWeatherJson.main.temp_max).toFixed(2) + '°',
+      'temp_min': (resWeatherJson.main.temp_min).toFixed(2) + '°',
+      'feels_like': (resWeatherJson.main.feels_like).toFixed(2) + '°',
+
+      'pressure': (resWeatherJson.main.pressure).toFixed(0) + ' hPa',
+      'humidity': resWeatherJson.main.humidity + '%',
+      'weatherdesc': resWeatherJson.weather[0].description,
+      'icon': resWeatherJson.weather[0].icon,
+      'winddeg': resWeatherJson.wind.deg + '°',
+      'windspeed': resWeatherJson.wind.speed,
+      'rain': resWeatherJson.rain?.['1h'],
+      'snow': resWeatherJson.snow?.['1h'],
+    })
+    const newWeather = new Weather1({ // create report
+      'city': resWeatherJson.name,
+      'temperature': (resWeatherJson.main.temp).toFixed(2),
+      'date': date,
+      'rain': resWeatherJson.rain?.['1h'],
+      'snow': resWeatherJson.snow?.['1h'],
+      'pressure': (resWeatherJson.main.pressure).toFixed(2) + ' hPa'
+    })
+    //const newWeather = new Weather()   //alternative for create report
+    //newWeather.city =  resWeatherJson.name
+
+    newWeather.save().then(() => console.log('I save record')); //commit
+  } else {
+    console.log('no data!')
+  }
+})
+
+//read
+app.get('/api/log', async (req, res) => {
+
+  let log = await Weather1.find()
+  res.json(log)
+})
+
+app.get('/api/send', async (req, res) => {
+
+  let send = await Okanswer.find()
+  res.json(send)
+  res.save();
+})
+
+//update
+app.get('/api/log1', async (req, res) => {
+  let log = await Weather1.find() // findOne() // findById(id)
+  log[1].city = 'PTZ'
+  log[1].save()
+})
+
+//delete
+// Weather.deleteMany // delete table
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
